7118588c85010e45845a3db7f77bde76c9850648
Leila <lejlapersoperso@gmail.com>
2026-01-14 23:24:21 +0100

    Add files via upload

---
+++ main (1).cpp
@@ -0,0 +1,35 @@
+/* 5. Write a program that finds and prints all of the prime numbers between 3 
+and 100. A prime number is a number such that 1 and itself are the only 
+numbers that evenly divide it (for example, 3, 5, 7, 11, 13, 17, …).
+ One way to solve this problem is to use a doubly nested loop. The 
+outer loop can iterate from 3 to 100 while the inner loop checks to 
+see if the counter value for the outer loop is prime. One way to see 
+if number n is prime is to loop from 2 to n 21 and if any of these 
+numbers evenly divides n, then n cannot be prime. If none of the 
+values from 2 to n 21 evenly divides n, then n must be prime. (Note 
+that there are several easy ways to make this algorithm more efficient.*/
+ #include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Prime numbers between 3 and 100 are:\n";
+
+    for (int n = 3; n <= 100; n++) {
+        bool isPrime = true;
+
+        // Check divisibility from 2 up to sqrt(n)
+        for (int i = 2; i * i <= n; i++) {
+            if (n % i == 0) {
+                isPrime = false; // Not prime
+                break;           // No need to keep checking
+            }
+        }
+
+        if (isPrime) {
+            cout << n << " ";
+        }
+    }
+
+    cout << endl;
+    return 0;
+}
---
+++ main.cpp
@@ -0,0 +1,56 @@
+/*10. Computer file sizes are measured in units of bytes, or higher units like 
+kilobytes, megabytes, or gigabytes. Bytes, kilobytes, megabytes, and 
+gigabytes are related by the following:
+ a. One kilobyte is equal to 1024 bytes.
+ b. One megabyte is equal to 1024 kilobytes.
+ c. One gigabyte is equal to 1024 megabytes.
+ Write a program that lets the user convert the size of a file from gigabytes, 
+megabytes, or kilobytes to bytes. The program should prompt the user to 
+enter the size of the file and the units the file size is being measured in, 
+with G for gigabytes, M for Megabytes and K for Kilobytes. The program 
+should then output the size of the file in each of the corresponding 
+smaller file size types. For example, if the user enters the file size in 
+megabytes, the program should output the file size in kilobytes and bytes. */
+#include <iostream>
+using namespace std;
+int main()
+{
+    double kilobytes = 1024.0;
+   // double megabytes = 2048.0;
+   // double gigabytes = 3096.0;
+    int size, kiloSize, megaSize, gigaSize;
+    char userUnit, convertedUnit;
+    
+    cout << "hii please fill in the following fields.\n";
+    cout << "current size of your file ";
+    cin >> size;
+    cout << "\ncurrent unit of your file ";
+    cin >> userUnit;
+    
+    userUnit = toupper(userUnit);
+    
+    switch(userUnit){
+        case 'G': 
+        megaSize = gigaSize * kilobytes;
+        cout << "giga to mega " << megaSize << endl;
+        kiloSize = gigaSize * 1048.5;
+        cout << "giga to kilo " << kiloSize << endl;
+        break;
+    
+       case 'M':
+       kiloSize = megaSize * kilobytes;
+       cout << "mega to kilo " << kiloSize << endl;
+       gigaSize = megaSize / kilobytes;
+       cout << "mega to giga " << gigaSize;
+       break;
+       
+       case 'K':
+       megaSize = kiloSize / kilobytes ;
+       cout << "kilo to mega" << megaSize << endl;
+       gigaSize = kiloSize / (kilobytes*kilobytes);
+       cout << "kilo to giga" << gigaSize;
+      // break;
+    }
+    
+    return 0;
+}
